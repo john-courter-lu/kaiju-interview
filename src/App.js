aeb6bf1ccc6206743e6f842e652c1e1e215ae2c0
John Lu <johnlu2006@gmail.com>
2023-11-16 19:06:26 -0600

    second step finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-// import { getKaiju } from './getKaiju';
+import { getKaiju } from './getKaiju';
 
 function App() {
   // simulates a fetch call
-  // getKaiju();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    getKaiju()
+      .then(
+        (Response) => Response.json())
+      .then((res) => { setData(res.default) })
+  }, [])
+
+  console.log(data);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Kaiju Search</h1>
+        <ul>
+          {data.map(obj =>
+            <li>{obj.name}</li>
+          )}
+        </ul>
       </header>
     </div>
   );
